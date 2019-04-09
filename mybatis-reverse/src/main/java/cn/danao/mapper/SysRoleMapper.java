820d3b17385335713f3763bbeedacb05cdbf82c3
cooix <zhangyanmingjiayou@163.com>
2019-04-09 11:23:37 +0800

    code(添加安全权限框架security) 权限管理

---
+++ mybatis-reverse/src/main/java/cn/danao/mapper/SysRoleMapper.java
@@ -0,0 +1,22 @@
+package main.java.cn.danao.mapper;
+
+import java.util.List;
+import main.java.cn.danao.dao.SysRole;
+import main.java.cn.danao.dao.SysRoleExample;
+import org.apache.ibatis.annotations.Param;
+
+public interface SysRoleMapper {
+    int countByExample(SysRoleExample example);
+
+    int deleteByExample(SysRoleExample example);
+
+    int insert(SysRole record);
+
+    int insertSelective(SysRole record);
+
+    List<SysRole> selectByExample(SysRoleExample example);
+
+    int updateByExampleSelective(@Param("record") SysRole record, @Param("example") SysRoleExample example);
+
+    int updateByExample(@Param("record") SysRole record, @Param("example") SysRoleExample example);
+}
---
+++ mybatis-reverse/src/main/java/cn/danao/mapper/SysRoleUserMapper.java
@@ -0,0 +1,30 @@
+package main.java.cn.danao.mapper;
+
+import java.util.List;
+import main.java.cn.danao.dao.SysRoleUser;
+import main.java.cn.danao.dao.SysRoleUserExample;
+import org.apache.ibatis.annotations.Param;
+
+public interface SysRoleUserMapper {
+    int countByExample(SysRoleUserExample example);
+
+    int deleteByExample(SysRoleUserExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SysRoleUser record);
+
+    int insertSelective(SysRoleUser record);
+
+    List<SysRoleUser> selectByExample(SysRoleUserExample example);
+
+    SysRoleUser selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") SysRoleUser record, @Param("example") SysRoleUserExample example);
+
+    int updateByExample(@Param("record") SysRoleUser record, @Param("example") SysRoleUserExample example);
+
+    int updateByPrimaryKeySelective(SysRoleUser record);
+
+    int updateByPrimaryKey(SysRoleUser record);
+}
