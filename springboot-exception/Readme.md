9f7b1886c53a4a51c273beec4cc6cab3db22b1e1
zhangyanmingjiayou@163.com <zhangyanmingjiayou@163.com>
2019-05-09 21:50:29 +0800

    code(exceptrion) add class

--- springboot-exception/Readme.md
+++ springboot-exception/Readme.md
@@ -1,2 +1,4 @@
 # springboot的全局异常处理
 
+- spring 的异常处理体系
+
---
+++ springboot-exception/src/main/java/com/danao/exception/GlobalException.java
@@ -0,0 +1,17 @@
+package com.danao.exception;
+
+import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
+
+/**
+ * @author <a href="mailto:zhangyanmingjiayoiu@163.com">Thinkpad</a>
+ * @version 1.0
+ * @classname GlobalException
+ * @descriptionclass
+ * 1.全局异常处理类
+ * 2.****************
+ * @createdate 2019/5/9 9:17
+ * @since 1.0
+ */
+public class GlobalException {
+
+}
