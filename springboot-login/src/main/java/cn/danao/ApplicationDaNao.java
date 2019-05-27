78d4dc22503169706465f5aaadd4a51e3db615db
zhangym <zhangyanmingjiayou@163.com>
2019-05-27 10:49:12 +0800

    alter login coding

--- springboot-login/src/main/java/cn/danao/ApplicationDaNao.java
+++ springboot-login/src/main/java/cn/danao/ApplicationDaNao.java
@@ -1,9 +1,10 @@
 package cn.danao;
 
-import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class ApplicationDaNao {
     public static void main(String[] args) {
--- springboot-login/src/main/java/cn/danao/controller/HelloController.java
+++ springboot-login/src/main/java/cn/danao/controller/HelloController.java
@@ -1,17 +1,19 @@
 package cn.danao.controller;
 
-import cn.danao.bean.UserInfo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.*;
 
+@Api("登陆测试接口")
 @RestController
 public class HelloController {
     Logger logger = LoggerFactory.getLogger(HelloController.class);
 
-    @RequestMapping(value = "/test")
+    @ApiOperation("测试接口")
+    @RequestMapping(value = "/test",method = RequestMethod.GET)
     public String  test(){
         logger.info("请求路由为:test"  );
         return "test";
