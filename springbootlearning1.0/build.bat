89e3e706426b2bf8386c04310d0d5d18705f8d98
zhangym <zhangyanmingjiayou@163.com>
2019-08-02 18:34:17 +0800

    add windows script

--- springbootlearning1.0/build.bat
+++ springbootlearning1.0/build.bat
@@ -5,8 +5,4 @@ echo "================================= clean success ==========================
 echo "================================= package start ================================="
 call mvn package -Dmaven.test.skip=true
 echo "================================= package success ================================="
-echo "================================= porject start ================================="
-set dirPath=%cd%/target/springbootlearingone-1.0-SNAPSHOT.jar
-echo %dirPath%
-java -jar %dirPath%
 
---
+++ springbootlearning1.0/start.bat
@@ -0,0 +1,5 @@
+@echo off
+echo "================================= porject start ================================="
+set dirPath=%cd%/target/springbootlearingone-1.0-SNAPSHOT.jar
+echo start jar is %dirPath%
+java -jar %dirPath%
