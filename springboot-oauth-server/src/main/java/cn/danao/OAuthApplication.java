62f6fc81c3e21d99c0c8658ee514f8e892602271
zhangym <zhangyanmingjiayou@163.com>
2019-10-15 15:16:55 +0800

    add druid html

---
+++ springboot-oauth-server/src/main/java/cn/danao/OAuthApplication.java
@@ -0,0 +1,21 @@
+package cn.danao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author danao
+ * @version 1.0
+ * @classname OAuthApplication
+ * @descriptionclass 1.类的作用
+ * 2.其他说明
+ * @createdate 2019/10/15 15:09
+ * @since 1.0
+ */
+@SpringBootApplication
+public class OAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuthApplication.class,args);
+    }
+}
---
+++ springboot-oauth-server/src/main/java/cn/danao/controller/StartController.java
@@ -0,0 +1,120 @@
+package cn.danao.controller;
+
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author danao
+ * @version 1.0
+ * @classname StartController
+ * @descriptionclass 1.测试基本项目
+ * 2.其他说明
+ * @createdate 2019/10/15 15:11
+ * @since 1.0
+ */
+@RestController
+@RequestMapping(value = "/start")
+@Slf4j
+public class StartController {
+
+    /**
+     * 测试项目是否启动
+     *
+     * @return
+     */
+    @RequestMapping(value = "/success")
+    public String isStart() {
+        log.info("测试项目是否启动");
+        Map<String, Object> result = new HashMap<>();
+        result.put("status", "200");
+        result.put("msg", "测试成功");
+        return JSON.toJSONString(result);
+    }
+
+    @RequestMapping(value = "/request/test", method = RequestMethod.POST)
+    public String testRequest() {
+        return "success";
+    }
+
+    /**
+     * 测试get   请求参数
+     * 需要两个参数 params:value 和 num:数字 类型与后台代码一致
+     *
+     * @param argOne
+     * @param argTwo
+     * @return
+     */
+    @RequestMapping(value = "/params", method = RequestMethod.GET)
+    public String isParams(@RequestParam String argOne, @RequestParam int argTwo) {
+        log.info("请求的是参数测试接口，请求参数 [ {} {} ]", argOne, argTwo);
+        Map<String, Object> params = new HashMap<>();
+        params.put("status", 200);
+        params.put("msg", "success");
+        params.put("result", "请求参数，argOne = " + argOne + ",argTwo = " + argTwo);
+        return JSON.toJSONString(params);
+    }
+
+    /**
+     * post发送json请求的测试接口
+     *
+     * @param params
+     * @return
+     */
+    @RequestMapping(value = "/params/post/json", produces = {"application/json;charset=UTF-8"}, method = RequestMethod.POST)
+    public String isParamsPostJson(@RequestBody Map<String, Object> params) {
+        log.info("请求的是参数测试接口，请求参数 [{}]", params);
+        params.put("status", "200");
+        params.put("msg", "测试成功");
+        return JSON.toJSONString(params);
+    }
+
+    /**
+     * post发送json请求的测试接口
+     *
+     * @param argOne
+     * @param argTwo
+     * @return
+     */
+    @RequestMapping(value = "/params/post", method = RequestMethod.POST)
+    public String isParamsPost(@RequestParam String argOne, @RequestParam String argTwo) {
+        log.info("请求的是参数测试接口，请求参数 [{} {}]", argOne, argTwo);
+        Map<String, Object> params = new HashMap<>();
+        params.put("status", "200");
+        params.put("msg", "测试成功" + "参数" + argOne + "," + argTwo);
+        return JSON.toJSONString(params);
+    }
+
+    /**
+     * 上传用Post 关键字名 file 数据类型 文件
+     * 问题 获取到的文件名不正确
+     *
+     * @param fileUpload
+     * @return
+     */
+    @RequestMapping(value = "/commit/file", method = RequestMethod.POST)
+    public String commitFile(@RequestParam("file") MultipartFile fileUpload) {
+        try {
+            if (fileUpload != null) {
+                String fileName = fileUpload.getName();
+                String fileNames = fileUpload.getOriginalFilename();
+                log.info(fileName + " : " + fileNames);
+                //默认参数对
+                Map<String, Object> params = new HashMap<String, Object>();
+                params.put("file", fileUpload);
+                //file
+                params.put("file", fileUpload);
+                log.info("+++++++++++++" + fileName + fileUpload.isEmpty());
+            }
+            return "success";
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "false";
+    }
+
+}
