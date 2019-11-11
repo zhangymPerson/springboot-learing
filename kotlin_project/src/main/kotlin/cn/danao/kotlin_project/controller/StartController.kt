d3789dd480d2eaa563867af17850bb4e739f923c
zhangym <zhangyanmingjiayou@163.com>
2019-11-11 11:48:30 +0800

    add interface and service test code

--- kotlin_project/src/main/kotlin/cn/danao/kotlin_project/controller/StartController.kt
+++ kotlin_project/src/main/kotlin/cn/danao/kotlin_project/controller/StartController.kt
@@ -1,9 +1,10 @@
 package cn.danao.kotlin_project.controller
 
-import lombok.extern.slf4j.Slf4j
+import cn.danao.kotlin_project.server.StartServer
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.annotation.Resource
 
 @RestController
 //kotlin目前不能使用@Slfj注解
@@ -11,12 +12,21 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping(value = ["/start"])
 class StartController {
 
+
+    @Resource
+    lateinit var startServer: StartServer;
+    var s : Any = startServer.getSum(12,21)
+
     //创建日志
     var log = LoggerFactory.getLogger(this.javaClass);
+
     @RequestMapping(value = ["/test"])
     fun testRequest(name: String): String {
         log.info("name = ${name}")
         println("name = ${name}")
+        var sum = startServer.getSum(12, 14)
+        var test = startServer.interfaceTest();
+        log.info("sum = ${sum}, test = ${test}")
         return "测试"
     }
 
---
+++ kotlin_project/src/main/kotlin/cn/danao/kotlin_project/server/impl/StartServerImpl.kt
@@ -0,0 +1,27 @@
+package cn.danao.kotlin_project.server.impl
+
+import cn.danao.kotlin_project.server.StartServer
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+
+@Service
+class StartServerImpl :StartServer{
+    var log = LoggerFactory.getLogger(this.javaClass);
+    override fun getSum(a: Int, b: Int, c: String): String {
+        /**
+         *TODO("not implemented")”
+         * 这句代码，这句代码在运行的时候会抛出以上异常，
+         * 这样做的好处是促使我们去实现接口方法或者手动删掉这句代码。
+         * 解决这个异常的方法就是删掉这行代码
+         */
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        log.info("请求服务参数是 a = ${a}, b =  ${b},c = ${c}")
+        return (a+b).toString()
+    }
+
+    override fun interfaceTest(): String {
+        log.info("测试接口实现方法的实现")
+        return super.interfaceTest()
+    }
+
+}
