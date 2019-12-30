52d60ef22fdf3a098a631605aae7000d87d1e401
zhangym <zhangyanmingjiayou@163.com>
2019-12-30 17:37:59 +0800

    alter code

---
+++ springboot-shiro/src/main/java/cn/danao/springbootshiro/cong/ShiroConfig.java
@@ -0,0 +1,36 @@
+package cn.danao.springbootshiro.cong;
+
+import org.apache.shiro.authc.Authenticator;
+import org.apache.shiro.authc.pam.FirstSuccessfulStrategy;
+import org.apache.shiro.authc.pam.ModularRealmAuthenticator;
+import org.apache.shiro.mgt.SessionStorageEvaluator;
+import org.apache.shiro.realm.Realm;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
+import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
+import org.apache.shiro.web.mgt.DefaultWebSessionStorageEvaluator;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import java.util.Arrays;
+import java.util.Map;
+
+
+/**
+ * date 2019/12/30 11:36 <br/>
+ * descriptionclass <br/>
+ * 权限配置
+ *
+ * @author danao
+ * @version 1.0
+ * @since 1.0
+ */
+@Configuration
+public class ShiroConfig {
+
+
+
+}
