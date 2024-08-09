a104e4f8644d5af74bebd4c1acec07954812c9e1
zhangyanming <zhangyanmingjiayou@163.com>
2024-08-09 16:06:05 +0800

    feat: 修改 sql

--- src/main/resources/db/migration/V1__Add_new_user.sql
+++ src/main/resources/db/migration/V1__Add_new_user.sql
@@ -1,7 +1,7 @@
 create table if not exists user (
  id int auto_increment primary key,
- create_time datetime not null default current_timestamp,
- create_by varchar(255) not null,
+ create_time datetime default current_timestamp,
+ create_by varchar(255) default null,
  update_time datetime default null on update current_timestamp,
  update_by varchar(255) default null,
  remark text
